a558f380335b6d5afe2a93ccecda8942e74157b9
Nun3s01 <cauanunes77@yahoo.com>
2025-12-19 13:17:10 -0300

    Correção: Arquitetura de Deployment - Base & Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://nun3s01.github.io/',
+    base: '/portfolio-page-3.0',
+});

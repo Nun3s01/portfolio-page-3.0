aa796c094ae9f5e1c86fd740b1765beeeb389385
Nun3s01 <cauanunes77@yahoo.com>
2025-12-24 17:10:59 -0300

    Add Hero Section in Main Index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
-    site: 'https://nun3s01.github.io/',
-    base: '/portfolio-page-3.0',
+  site: 'https://nun3s01.github.io/',
+  base: '/portfolio-page-3.0',
+  integrations: [react()],
 });
